060a314a51fd725e94889e701253feb81d1963fa
Ignacio <ignaciomendez6@gmail.com>
2024-06-04 10:19:57 -0300

    first commit

---
+++ Pokemon/Pokemon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Nacho Mendez on 04/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
